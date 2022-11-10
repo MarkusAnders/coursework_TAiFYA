c3b9807efd1b6a60638490919f500cc80156da1c
MarkusAnders <ser.play02@mail.ru>
2022-11-10 09:20:45 +0300

    Добавление ТТС таблицы

--- Form1.cs
+++ Form1.cs
@@ -22,9 +22,82 @@ namespace coursework_TAiFYA
 
 		private void PerformToLexemAnalys_Click(object sender, EventArgs e)
 		{
-			lexemes lxms = new lexemes();
-			var lexemes = lxms.Check(TextMain.Text);
-			firstTable.DataSource = lexemes;
-		}
+            lexemes lxms = new lexemes();
+            lemexeClass lxmscls = new lemexeClass();
+            var lexemes = lxms.Check(TextMain.Text);
+            var tcc = lxmscls.Classify(lexemes);
+            firstTable.DataSource = lexemes;
+            tccTable.DataSource = tcc;
+
+            if (!keywordTable.Columns.Contains("Keywords"))
+            {
+                keywordTable.Columns.Add("Keywords", "Ключевые слова");
+                foreach (var i in lxmscls.keywords)
+                {
+                    keywordTable.Rows.Add(i.ToString());
+                }
+            }
+            else
+            {
+                keywordTable.Rows.Clear();
+                foreach (var i in lxmscls.keywords)
+                {
+                    keywordTable.Rows.Add(i.ToString());
+                }
+            }
+
+
+            if (!indifTable.Columns.Contains("Identifier"))
+            {
+                indifTable.Columns.Add("Identifier", "Индификатор");
+
+                foreach (var i in lxmscls.identifiers)
+                {
+                    indifTable.Rows.Add(i.ToString());
+                }
+            }
+            else
+            {
+                indifTable.Rows.Clear();
+                foreach (var i in lxmscls.identifiers)
+                {
+                    indifTable.Rows.Add(i.ToString());
+                }
+            }
+
+            if (!literalTable.Columns.Contains("Litaral"))
+            {
+                literalTable.Columns.Add("Litaral", "Литерал");
+                foreach (var i in lxmscls.literals)
+                {
+                    literalTable.Rows.Add(i.ToString());
+                }
+            }
+            else
+            {
+                literalTable.Rows.Clear();
+                foreach (var i in lxmscls.literals)
+                {
+                    literalTable.Rows.Add(i.ToString());
+                }
+            }
+
+            if (!separatorTable.Columns.Contains("Separators"))
+            {
+                separatorTable.Columns.Add("Separators", "Разделитель");
+                foreach (var i in lxmscls.separators_list)
+                {
+                    separatorTable.Rows.Add(i.ToString());
+                }
+            }
+            else
+            {
+                separatorTable.Rows.Clear();
+                foreach (var i in lxmscls.separators_list)
+                {
+                    separatorTable.Rows.Add(i.ToString());
+                }
+            }
+        }
 	}
 }
